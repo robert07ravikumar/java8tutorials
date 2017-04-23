0bd389d5229d9ff318a3485f7a652771a5281d6a
Dell\Mabel <robert07ravikumar@gmail.com>
2017-04-23 15:28:57 +0400

    sorting woth reversed

--- src/com/java8/TestSorting.java
+++ src/com/java8/TestSorting.java
@@ -3,6 +3,7 @@ package com.java8;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -25,17 +26,30 @@ public class TestSorting {
 		
 		developers.forEach((dev)->System.out.println(dev));
 		
+		System.out.println("sort by name ");
+		
+		developers.sort((o1,o2)->o1.getName().compareTo(o2.getName()));
+		
+		developers.forEach((dev)->System.out.println(dev));
+		
+		System.out.println("reversed sorting");
+		
+		Comparator<Developer> salaryComp = (o1,o2) -> o1.getSalary().compareTo(o2.getSalary());
+		developers.sort(salaryComp.reversed());
+		
+		developers.forEach(System.out::println);
+		
 	}
 	
 	private static List<Developer> getDevelopers(){
 		
 		List<Developer> devs = new ArrayList<Developer>();
 		
-		devs.add(new Developer(31, "robert", new BigDecimal(1231231423)));
-		devs.add(new Developer(23, "ravi", new BigDecimal(1231231423)));
-		devs.add(new Developer(46, "kumar", new BigDecimal(1231231423)));
-		devs.add(new Developer(52, "anand", new BigDecimal(1231231423)));
-		devs.add(new Developer(21, "richie", new BigDecimal(1231231423)));
+		devs.add(new Developer(31, "robert", new BigDecimal(5523)));
+		devs.add(new Developer(23, "ravi", new BigDecimal(3231)));
+		devs.add(new Developer(46, "kumar", new BigDecimal(2314)));
+		devs.add(new Developer(52, "anand", new BigDecimal(8231)));
+		devs.add(new Developer(21, "richie", new BigDecimal(7398)));
 		
 		return devs;
 	}
